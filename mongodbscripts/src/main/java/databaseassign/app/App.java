a6038a604184fab521cc2b1354b56abd4d4bc516
Jack Menjivar <jackstar710@gmail.com>
2018-03-24 09:58:38 +0000

    Simple File Reading

--- mongodbscripts/src/main/java/databaseassign/app/App.java
+++ mongodbscripts/src/main/java/databaseassign/app/App.java
@@ -21,27 +21,43 @@ import static com.mongodb.client.model.Updates.*;
 import com.mongodb.client.result.UpdateResult;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Scanner;
+import java.io.File;
 
 public class App
 {
     public static void main( String[] args )
     {
-        MongoClient mongoClient = new MongoClient();
-	MongoDatabase database = mongoClient.getDatabase("myDatabase");
-	MongoCollection<Document> collection = database.getCollection("myCollection");
+	try
+	{
+	    String csvFile1 = "/home/ec2-user/test.csv";
+	    Scanner sc = new Scanner(new File(csvFile1));
+	    while(sc.hasNextLine())
+	    {
+		String line = sc.nextLine();
+		System.out.println(line);
+	    }
+	}
+	catch (Exception e)
+	{
+	    e.printStackTrace();
+	}
+        //MongoClient mongoClient = new MongoClient();
+	//MongoDatabase database = mongoClient.getDatabase("myDatabase");
+	//MongoCollection<Document> collection = database.getCollection("myCollection");
 	//List<Document> documents = new ArrayList<Document>();
-	System.out.println(collection.count());
+	//System.out.println(collection.count());
 	    /*for (int i = 0; i < 100; i++) {
 	    documents.add(new Document("i", i));
-	}
-	collection.insertMany(documents);
-	Document doc = new Document("name", "MongoDB")
+	    }
+	    collection.insertMany(documents);*/
+	/*Document doc = new Document("name", "MongoDB")
 	    .append("type", "database")
 	    .append("count", 1)
 	    .append("versions", Arrays.asList("v3.2", "v3.0", "v2.6"))
 	    .append("info", new Document("x", 203).append("y", 102));
-	collection.insertOne(doc);		
-	System.out.println(doc.toJson());
-	System.out.println(myDoc.toJson());*/
+	    collection.insertOne(doc);		
+	System.out.println(collection.count());
+	System.out.println(doc.toJson());*/
     }
 }
